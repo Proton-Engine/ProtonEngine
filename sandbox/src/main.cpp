ca0bb364edee4878df42c8b76c40bbfb22224098
Tim Herreijgers <timtim3001@gmail.com>
2022-10-25 19:31:09 +0200

    Added sandbox project

---
+++ sandbox/src/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright © 2022 Tim Herreijgers
+ * Licensed using the MIT license
+ */
+
+#include "protonengine/core/entrypoint.h"
+#include "protonengine/core/application.h"
+
+#include <fmt/core.h>
+
+class SandboxApplication : public ProtonEngine::Core::Application
+{
+public:
+    void initialize() override
+    {
+        fmt::print("Hello world!\n");
+    }
+};
+
+std::unique_ptr<ProtonEngine::Core::Application> getProtonApplication(int argc, char **argv)
+{
+    return std::make_unique<SandboxApplication>();
+}
+
